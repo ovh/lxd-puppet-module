112ac65d4cd90d7ce5ea0ae90d3efe9c98b10aff
night199uk <night199uk@hermitcrabslab.com>
2021-11-06 11:17:32 -0700

    Fix: correctly allow to set server properties.

--- manifests/config.pp
+++ manifests/config.pp
@@ -8,7 +8,9 @@ class lxd::config{
     # params from main module
     $lxd_auto_update_interval = $lxd::lxd_auto_update_interval
     $lxd_auto_update_interval_ensure = $lxd::lxd_auto_update_interval_ensure
+    $lxd_core_https_address = $lxd::lxd_core_https_address
     $lxd_core_https_address_ensure = $lxd::lxd_core_https_address_ensure
+    $lxd_core_trust_password = $lxd::lxd_core_trust_password
     $lxd_core_trust_password_ensure = $lxd::lxd_core_trust_password_ensure
 
     lxd_config { 'global_images.auto_update_interval':
@@ -20,10 +22,12 @@ class lxd::config{
     lxd_config { 'global_core.https_address':
         ensure => $lxd_core_https_address_ensure,
         config => [ 'core.https_address',],
+        value  => $lxd_core_https_address,
     }
 
     lxd_config { 'global_core.trust_password':
         ensure => $lxd_core_trust_password_ensure,
         config => [ 'core.trust_password',],
+        value  => $lxd_core_trust_password,
     }
 }
