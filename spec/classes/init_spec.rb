61faba9133300f05ab763f2cc69cc5468b606337
Alexandar Yovchev <alexyov@protonmail.com>
2022-01-10 16:34:31 +0200

    Fix lxd::init test
    * Included tests for all supported OS's as per metadata.json
    * Add test to validate install & uninstall order
    * Fix tests for deb & snap installs

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -4,51 +4,90 @@
 
 require 'spec_helper'
 
-describe '::lxd' do
+describe 'lxd' do
+    on_supported_os.each do | os, facts |
+        context "on #{os}" do
+            let(:facts) do
+                facts
+            end
 
-    let(:params) {{
-        'lxd_auto_update_interval' => 0,
-    }}
+            describe 'with ensure => present' do
+                let(:params) do
+                    {
+                        'ensure' => 'present',
+                        'lxd_auto_update_interval' => 0,
+                    }
+                end
+                it do
+                    is_expected.to contain_class('lxd::install').that_comes_before(
+                        'Class[lxd::config]',
+                    )
+                end
+                it do
+                    is_expected.to contain_class('lxd::config')
+                end
+            end
 
-    it do
-        is_expected.to contain_class('lxd::install').that_comes_before('lxd::config')
-    end
-    it do
-        is_expected.to contain_class('lxd::config').that_comes_after('lxd::install')
-    end
+            describe 'with ensure => absent' do
+                let(:params) do
+                    {
+                        'ensure' => 'absent',
+                        'lxd_auto_update_interval' => 0,
+                    }
+                end
+                it do
+                    is_expected.to contain_class('lxd::install')
+                end
+                it do
+                    is_expected.to contain_class('lxd::config').that_comes_before(
+                        'Class[lxd::install]',
+                    )
+                end
 
-    context 'with install from deb package' do
-        let(:params) do
-            super().merge({
-                'provider' => 'deb',
-            })
-        end
-        # Test packages which should be installed
-        it { is_expected.to contain_package('lxd').with_ensure('present')}
-    end
-
-    context 'with install from snap' do
-        let(:params) do
-            super().merge({
-                'provider' => 'snap',
-            })
-        end
-        it do
-            is_expected.to contain_package('snapd').with_ensure('present')
-        end
-        it do
-            is_expected.to contain_exec('install snap core').with(
-                'path' => '/usr/bin:/bin',
-                'command' => '/usr/bin/snap install core',
-                'unless' => '/usr/bin/snap list core',
-            ).that_requires('Package[snapd]')
-        end
-        it do
-            is_expected.to contain_exec('install lxd').with(
-                'path' => '/usr/bin:/bin',
-                'command' => '/usr/bin/snap install lxd',
-                'unless' => '/usr/bin/snap list lxd',
-            ).that_requires('Exec[install snap core]')
+                describe 'with package install' do
+                    let(:params) do
+                        super().merge(
+                            {
+                                'provider' => 'deb',
+                            }
+                        )
+                    end
+                    it do
+                        is_expected.to contain_package('lxd').with(
+                            'ensure' => 'absent',
+                            'install_options' => [],
+                        )
+                    end
+                    it { is_expected.not_to contain_exec('install snap core') }
+                    it { is_expected.not_to contain_exec('install lxd') }
+                end
+                describe 'with snap install' do
+                    let(:params) do
+                        super().merge(
+                            {
+                                'provider' => 'snap',
+                            }
+                        )
+                    end
+                    it do
+                        is_expected.to contain_package('snapd').with_ensure('absent')
+                    end
+                    it do
+                        is_expected.to contain_exec('install snap core').with(
+                            'path' => '/usr/bin:/bin',
+                            'command' => '/usr/bin/snap install core',
+                            'unless' => '/usr/bin/snap list core',
+                        ).that_requires('Package[snapd]')
+                    end
+                    it do
+                        is_expected.to contain_exec('install lxd').with(
+                            'path' => '/usr/bin:/bin',
+                            'command' => '/usr/bin/snap install lxd',
+                            'unless' => '/usr/bin/snap list lxd',
+                        ).that_requires('Exec[install snap core]')
+                    end
+                end
+            end
         end
     end
 end
